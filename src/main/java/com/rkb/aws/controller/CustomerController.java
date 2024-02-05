9848be1fe0ff83a5046b5cf99d570d645b70808c
Rajesh <rajesh.hofo@gmail.com>
2024-02-05 13:07:59 +0800

    fixing

--- src/main/java/com/rkb/aws/controller/CustomerController.java
+++ src/main/java/com/rkb/aws/controller/CustomerController.java
@@ -1,14 +1,14 @@
 package com.rkb.aws.controller;
 
-import java.util.List;
-
+import com.rkb.aws.data.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.rkb.aws.data.User;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
--- src/test/java/com/rkb/aws/handler/LambdaHandlerTest.java
+++ src/test/java/com/rkb/aws/handler/LambdaHandlerTest.java
@@ -1,15 +1,14 @@
 package com.rkb.aws.handler;
 
-import java.io.IOException;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder;
 import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 class LambdaHandlerTest {
